369807da316d4f6bd959c19f871101e562897538
Mamadou KANOUTE <78961491+kanoutemamadou@users.noreply.github.com>
2023-10-26 23:29:34 +0200

    Add files via upload

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Analyse de Fourier sur le pouce",
+        "excerpt":"Ce post est un rapide aperçu de notions basiques sur les séries de Fourier et sur la transformée de Fourier.     ","categories": [],
+        "tags": [],
+        "url": "/fourier/",
+        "teaser": null
+      },{
+        "title": "Analyse de Fourier sur l'index",
+        "excerpt":"On va voir rapidement dans cette note comment élargir la transformée de Fourier à des objets plus généraux.     ","categories": [],
+        "tags": [],
+        "url": "/fourier2/",
+        "teaser": null
+      }]
